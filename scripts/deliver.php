225028949316d15ec7b831ae5f7530242f60e2c9
RumGuru <Rahul.kothwal@hotmail.com>
2021-02-28 10:54:14 -0500

    UI

--- scripts/deliver.php
+++ scripts/deliver.php
@@ -11,27 +11,56 @@
 
 <body>
   <?php include 'header.php'; ?>
-  <div class="container">
-    <h1 class="">Plan your trip</h1>
+  
+  <div>
+    
+    <h1 class="">Plan your Delivery</h1>
     <br>
-    <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle w-100" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Available Cars
-      </button>
-      <div class="dropdown-menu w-100" aria-labelledby="dropdownMenuButton">
-        <a class="dropdown-item " href="#">Tesla Model S - Price: CAD$100/km</a>
-        <a class="dropdown-item " href="#">Tesla Model 3 - Price: CAD$150/km</a>
-        <a class="dropdown-item " href="#">Tesla Model X - Price: CAD$200/km</a>
-        <a class="dropdown-item " href="#">Tesla Model Y - Price: CAD$250/km</a>
-      </div>
-    </div>
+    <div class='row'>
+        <div class='col-10'>
+          
+          <div class="card shop-item" style="width: 10rem;" draggable="true" >
+                <img class="card-img-top img-fluid" src="https://source.unsplash.com/PAEwrnasOvY/301x200" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Bonsai Tree</h4>
+                    <p class="card-text">Refer to Mr.Miyagi for trimming instructions</p>
+                    <p class="card-text">
+                        <small class="text-muted">$9.99</small>
+                    </p>
+                </div>
+          </div>
+            
+            <div class="card shop-item" style="width: 10rem;" draggable="true" >
+                <img class="card-img-top img-fluid" src="https://source.unsplash.com/PAEwrnasOvY/301x200" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Bonsai Tree</h4>
+                    <p class="card-text">Refer to Mr.Miyagi for trimming instructions</p>
+                    <p class="card-text">
+                        <small class="text-muted">$9.99</small>
+                    </p>
+               </div>
+            </div>
+        </div>
+        
+         <div class="col-2 shop-cart" style="background-color: #F3F3F3">
+          <h6>Shopping cart</h6>
+        </div>
+     </div>
+  
+
+    
   </div>
+
+
+
 
   <?php include 'footer.php'; ?>
 
+  <script src="../drag-drop.js"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  
 </body>
 
 </html>
