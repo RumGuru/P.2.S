dd95cea66d66128287a6dfa6a1b8ec6b5e46f15d
Filipe Gorodscy <fgorodscy@ryerson.ca>
2021-03-02 15:35:22 -0500

    Remove index.html

---
+++ pages/footer.php
@@ -0,0 +1,49 @@
+<footer id="footer" class="fixed-bottom bg-light text-center text-lg-start">
+  <div class="container p-4">
+    <div class="row">
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">About Us</h5>
+
+        <p>
+          P2S is an online system that aims to plan for smart green trips inside the city and its neighborhood
+          through sharing vehicles. P2S attempts to provide a smart green solution on this regard by matching up drivers who live, work,
+          and finally drive in the same neighborhood and would like to provide trip services.
+        </p>
+      </div>
+
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Developers</h5>
+
+        <p>
+        <div class="media">
+          <img class="align-self-center mr-3 developers" src="../images/Gorodscy.jpg" alt="FilipeGorodscy">
+          <div class="media-body">
+            <h5 class="mt-0">Filipe Gorodscy</h5>
+            <p class="mb-0"><a href="https://github.com/FilipeGorodscy">github.com/FilipeGorodscy</a></p>
+          </div>
+        </div>
+        <div class="media">
+          <img class="align-self-center mr-3 developers" src="../images/RumGuru.png" alt="RumGuru">
+          <div class="media-body">
+            <h5 class="mt-0">Rahul LastName</h5>
+            <p class="mb-0"><a href="https://github.com/RumGuru">github.com/RumGuru</a></p>
+          </div>
+        </div>
+        <div class="media">
+          <img class="align-self-center mr-3 developers" src="../images/MatthewFranks.png" alt="MatthewFranks">
+          <div class="media-body">
+            <h5 class="mt-0">Matthew Franks</h5>
+            <p class="mb-0"><a href="https://github.com/mjfranks">github.com/mjfranks</a></p>
+          </div>
+        </div>
+        </p>
+      </div>
+
+    </div>
+  </div>
+
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2020 Copyright:
+    <a class="text-dark" href="https://github.com/RumGuru/P.2.S">P2S Github Repository</a>
+  </div>
+</footer>
---
+++ pages/ride.php
@@ -0,0 +1,114 @@
+<html>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+  <link rel="stylesheet" href="../styles/footer.css">
+  <title>CPS-ASSIGNMENT</title>
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+    }
+
+    #map {
+      height: 500px;
+      width: 100%;
+    }
+  </style>
+  <script>
+    function moveDown() {
+      $(".form-group").css({
+        "margin-top": "150px"
+      })
+    }
+
+    function moveUp() {
+      $(".form-group").css({
+        "margin-top": "0px"
+      })
+    }
+
+    function onFormChange() {
+      document.getElementById('result').innerHTML =
+        `<table class="table">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">Selected car</th>
+              <th scope="col">Source</th>
+              <th scope="col">Destination</th>
+              <th scope="col">Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>` + document.getElementById('cars').value + `</td>
+              <td>` + document.getElementById('source').value + `</td>
+              <td>` + document.getElementById('destination').value + `</td>
+              <td>$500</td>
+            </tr>
+            </tbody>
+          </table>`
+    }
+
+    function onFormSubmit(e) {
+      let info = document.getElementById("info");
+      e.preventDefault();
+      info.innerHTML = `
+      <div class="alert alert-success alert-dismissible fade show" role="alert">
+        Your trip was added to the shopping cart!
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>`;
+      setTimeout(() => {
+        info.innerHTML = ""
+      }, 2000);
+    }
+  </script>
+</head>
+
+<body>
+  <?php include 'header.php'; ?>
+  <div class="container">
+    <h1 class="">Plan your trip</h1>
+    <br>
+    <form id="form" onsubmit="onFormSubmit(event)" onchange="onFormChange()">
+      <div class="form-group">
+        <label for="cars">Available cars</label>
+        <select class="form-control" id="cars">
+          <option selected>Choose...</option>
+          <option value="Tesla Model S">Tesla Model S - Price: CAD$10/km</option>
+          <option value="Tesla Model 3">Tesla Model 3 - Price: CAD$15/km</option>
+          <option value="Tesla Model X">Tesla Model X - Price: CAD$20/km</option>
+          <option value="Tesla Model Y">Tesla Model Y - Price: CAD$25/km</option>
+        </select>
+        <br>
+        <label for="source">Source</label>
+        <input onchange="initMap()" class="form-control" id="source" placeholder="Enter source address">
+        <br>
+        <label for="destination">Destination</label>
+        <input onchange="calcRoute()" class="form-control" id="destination" placeholder="Enter destination address">
+      </div>
+      <br>
+      <div id="result"></div>
+      <div id="map"></div>
+      <br>
+      <input type="submit" class="btn btn-secondary w-100" value="Add to shopping cart">
+      <br>
+      <br>
+      <div id="info"></div>
+    </form>
+  </div>
+  <?php include 'footer.php'; ?>
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCT0erosDR5II_8-FtZMdtCjCC_o5p2msE&callback=initMap">
+  </script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="../scripts/ride.js"></script>
+</body>
+
+</html>
